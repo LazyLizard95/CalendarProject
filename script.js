67e2e30e24618e681b7ef33801b3a7b921b3478c
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-28 16:57:20 -0400

    colors update
    switch statements cannot be used for maths. Had to use if

--- script.js
+++ script.js
@@ -3,40 +3,28 @@ $(document).ready(function() {
     $("#currentDay").append(date);
     
 });
-
 let hour = moment().format("H")
-// let timeValue = $(".timeValue");
 let timeValue = $(".timeValue").map(function(){
     return this.id;
 }).get();
-
-
 function checkTime(timeValue){
-                for(let i = 0; i < timeValue.length; i++){
-                console.log(hour);    
-                console.log(timeValue[i]);
-  
-switch (hour, timeValue[i]){
-        case (hour < timeValue[i]):
+    for(let i = 0; i < timeValue.length; i++){
+        if (hour < timeValue[i]){
+            $(".timeValue").addClass("past");
             console.log("hour is less than timevalue");
-        break;
-        case (hour > timeValue[i]):
+        }
+        if (hour > timeValue[i]){
+            $(".timeValue").addClass("future");
             console.log("hour is greater than timevalue");
-        break;
-        case (hour === timeValue[i]):
-            console.log("hour is equal to timevalue");
-        break;
-        default: console.log("NaN");
+        }
+        if (hour === timeValue[i]){
+            $(".timeValue").addClass("present");
+            console.log("hour is greater than timevalue");
+        }
     }
+
 }
-}   
-    checkTime(timeValue);
-
-
-
-
-
-
+checkTime(timeValue);
 
 
 
