54883454a334add861f2e7a3be9b41bdd1c37ce8
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-30 10:36:22 -0400

    why
    why

--- script.js
+++ script.js
@@ -7,20 +7,27 @@ let hour = moment().format("H")
 let timeValue = $(".timeValue").map(function(){
     return this.id;
 }).get();
+// console.log(hour)
+// console.log(timeValue)
 function checkTime(timeValue){
     for(let i = 0; i < timeValue.length; i++){
-        if (hour < timeValue[i]){
+            
+        if (hour > timeValue[i]){
             $(".timeValue").addClass("past");
             console.log("hour is less than timevalue");
         }
-        if (hour > timeValue[i]){
+        if (hour < timeValue[i]){
             $(".timeValue").addClass("future");
-            console.log("hour is greater than timevalue");
+            console.log(hour);
+            console.log(timeValue[i]);
+            console.log("hour is less than timevalue");
         }
         if (hour === timeValue[i]){
             $(".timeValue").addClass("present");
+            
             console.log("hour is greater than timevalue");
         }
+    
     }
 
 }
