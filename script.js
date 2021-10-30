7006a155bdb609b1da4909b4ab4510672ca90cf8
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-30 12:56:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
-    let date = moment().format("MM/DD/YYYY, H:mm, h:mm a");
+    let date = moment().format("MM/DD/YYYY, H:mm, h:mm a"); //set time and date to current
     $("#currentDay").append(date);
 });
-let hour = moment().format("H")
+
+let hour = moment().format("H") //variable for current hour
 let timeValue = $(".timeValue").map(function () {
-    return this.id;
+    return this.id; //create array with all timeValue id
 }).get();
 
 for (let i = 0; i < timeValue.length; i++) {
