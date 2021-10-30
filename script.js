f2b5526fe8000e698173d6691ccec5994b8dbe21
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-30 11:44:01 -0400

    needed to parse ints
    Was so frustrated, finally figured out comparing strings with strings... so i had to parse them in the comparasion

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let date = moment().format("MM/DD/YYYY, H:mm, h:mm a");
     $("#currentDay").append(date);
-    
 });
 let hour = moment().format("H")
-let timeValue = $(".timeValue").map(function(){
+let timeValue = $(".timeValue").map(function () {
     return this.id;
 }).get();
-// console.log(hour)
-// console.log(timeValue)
 
-    for(let i = 0; i < timeValue.length; i++){
-        parseInt(timeValue[i]);
-        parseInt(hour);
-        if (hour > timeValue[i]){
-            $("#" + timeValue[i]).addClass("past");
-            console.log("hour is less than timevalue");
-        }
-        if (hour < timeValue[i]){
-            $("#" + timeValue[i]).addClass("future");
-            console.log(hour);
-            console.log(timeValue[i]);
-            console.log("hour is less than timevalue");
-        }
-        if (hour === timeValue[i]){
-            $("#" + timeValue[i]).addClass("present");
-            
-            console.log("hour is greater than timevalue");
-        }
-    
+for (let i = 0; i < timeValue.length; i++) {
+    if (parseInt(hour) > parseInt(timeValue[i])) {
+        $("#" + timeValue[i]).addClass("past");
     }
+    if (parseInt(hour) < parseInt(timeValue[i])) {
+        $("#" + timeValue[i]).addClass("future");
+    }
+    if (parseInt(hour) === parseInt(timeValue[i])) {
+        $("#" + timeValue[i]).addClass("present");
+    }
+}
 
 
 
